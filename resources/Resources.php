c22079c2a42e8562b2675460f45d1c299aa246e0
jazinmendoza <32164218+jazinmendoza@users.noreply.github.com>
2017-09-21 21:18:24 +0800

    Add files via upload

---
+++ resources/Resources.php
@@ -0,0 +1,10 @@
+<?php
+    class Resources {
+        public $logo;
+        
+        public function __construct(){
+            
+            $this->$logo = "LoginLogov2.png";
+        }
+    }
+?>
